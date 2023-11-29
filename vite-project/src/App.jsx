d0bcb2f29dcbf36c3544112fa59752495245b838
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-11-29 14:57:39 +0100

    Add: Adding routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,6 +6,8 @@ import MovieBody from "./Components/Body/MovieBody";
 import Pricing from "./Components/Body/Pricing";
 import TvSection from "./Components/Body/TvSection";
 import Headers from "./Components/Headers/Headers";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
       <Celebs />
       <Pricing />
       <Footer />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
