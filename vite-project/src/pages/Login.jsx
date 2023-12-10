196a51c3a680f02272ebb150a776b1a2e19ddf64
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-12-10 06:33:58 +0100

    Add: Creating the sign in component

--- vite-project/src/pages/Login.jsx
+++ vite-project/src/pages/Login.jsx
@@ -5,29 +5,36 @@ const Login = () => {
   return (
     <>
       <main className={Classes["signin-main"]}>
+        <div className={Classes["logo"]}>
+          <h1>
+            Movi<span>x.</span>
+          </h1>
+        </div>
         <section className={Classes["signin-section"]}>
-          <h1>Sign in</h1>
-          <form className={Classes["Signin-form"]}>
-            <div className={Classes["email-phoneNumber"]}>
-              <input type="text" placeholder="Email or phone number" />
-            </div>
-            <div className={Classes["password-div"]}>
-              <input type="text" placeholder="Password" />
-            </div>
-            <button>Sign in</button>
-          </form>
-          <section className={Classes["enquiry-section"]}>
-            <aside>
-              <div className={Classes["checkbox"]}>
-                <input type="checkbox" />
+          <div className={Classes["signin-div"]}>
+            <h1>Sign in</h1>
+            <form className={Classes["Signin-form"]}>
+              <div className={Classes["email-phoneNumber"]}>
+                <input type="text" placeholder="Email or phone number" />
               </div>
-              <p>Remember me</p>
-            </aside>
-            <p>need help?</p>
-          </section>
-          <h2>
-            New to Movix? <p>Sign up now</p>{" "}
-          </h2>
+              <div className={Classes["password-div"]}>
+                <input type="text" placeholder="Password" />
+              </div>
+              <button>Sign in</button>
+            </form>
+            <section className={Classes["enquiry-section"]}>
+              <aside>
+                <div className={Classes["checkbox"]}>
+                  <input type="checkbox" />
+                </div>
+                <p>Remember me</p>
+              </aside>
+              <p>need help?</p>
+            </section>
+            <h2>
+              New to Movix? <p>Sign up now</p>{" "}
+            </h2>
+          </div>
         </section>
       </main>
     </>
