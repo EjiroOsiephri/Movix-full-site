97d91904d756db20ba31792948c104d280337950
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-12-13 09:16:23 +0100

    Add: Started sign  up component

--- vite-project/src/pages/Signup.jsx
+++ vite-project/src/pages/Signup.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Classes from "../Sass/Signup.module.scss";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <></>;
+  return (
+    <>
+      <main>
+        <header className={Classes["signup-header"]}>
+          <div className={Classes["signup-div"]}>
+            <h1>
+              MOVI<span>X</span>
+            </h1>
+            <h3>
+              <Link to="/signin">Sign in</Link>
+            </h3>
+            <hr />
+          </div>
+        </header>
+      </main>
+    </>
+  );
 };
 
 export default Signup;
