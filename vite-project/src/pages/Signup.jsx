6ee5104f68157e9c42d2927c71786eed91531915
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-12-13 09:44:47 +0100

    Add: Table property initiated

--- vite-project/src/pages/Signup.jsx
+++ vite-project/src/pages/Signup.jsx
@@ -5,12 +5,18 @@ import { Link, useNavigate } from "react-router-dom";
 const Signup = () => {
   const navigate = useNavigate();
 
+  const tableData = [
+    { id: 1, name: "John", age: 25 },
+    { id: 2, name: "Jane", age: 30 },
+    { id: 3, name: "Doe", age: 22 },
+  ];
+
   const navigateToSignInPage = () => {
     navigate("/");
   };
   return (
     <>
-      <main>
+      <main className={Classes["signup-main"]}>
         <header className={Classes["signup-header"]}>
           <div className={Classes["signup-div"]}>
             <h1 onClick={navigateToSignInPage}>
@@ -22,6 +28,26 @@ const Signup = () => {
           </div>
           <div className={Classes["Line-div"]}></div>
         </header>
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((row) => (
+                <tr key={row.id}>
+                  <td>{row.id}</td>
+                  <td>{row.name}</td>
+                  <td>{row.age}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </section>
       </main>
     </>
   );
