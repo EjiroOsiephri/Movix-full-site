eab613e7f56ce087911a62a21ac786055da66d3e
EjiroOsiephri <osiephriejiro765@gmail.com>
2024-01-26 10:36:09 +0100

    Add: Started google authentication

--- servers/server.js
+++ servers/server.js
@@ -1,14 +1,22 @@
 const fs = require("fs");
 const https = require("https");
 const express = require("express");
+const helmet = require("helmet");
 
 const PORT = 8000 || process.env.PORT;
 const app = express();
 
-const server = https.createServer({
-  key: fs.readFileSync("key.pem"),
-  cert: fs.readFileSync("cert.pem"),
-});
+const server = https.createServer(
+  {
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  },
+  app
+);
+
+//Middlewares
+
+app.use(helmet());
 
 app.get("/", (req, res) => {
   return res.send("Server started");
