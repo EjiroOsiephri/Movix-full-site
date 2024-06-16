a767fa5c91092580a4162fae742ae8c42449e5e9
ejiro osiephri <osiephriejiro765@gmail.com>
2024-06-16 22:47:37 +0100

    Added required dummy data for top-rated

--- vite-project/src/profile/components/SideBody.jsx
+++ vite-project/src/profile/components/SideBody.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Classes from "./SideBody.module.scss";
 
 const SideBody = () => {
   const genreData = [
@@ -13,7 +14,52 @@ const SideBody = () => {
     "Biography",
     "History",
   ];
-  return <></>;
+
+  const topratedData = [
+    {
+      name: "",
+      year: "",
+      genre: "",
+      rating: "",
+    },
+    {
+      name: "",
+      year: "",
+      genre: "",
+      rating: "",
+    },
+    {
+      name: "",
+      year: "",
+      genre: "",
+      rating: "",
+    },
+    {
+      name: "",
+      year: "",
+      genre: "",
+      rating: "",
+    },
+  ];
+
+  return (
+    <>
+      <main>
+        <section className={Classes["genre-section"]}>
+          <div className={Classes["genre-div"]}>
+            <h1>Genre</h1>
+            <h4>More</h4>
+          </div>
+          <div className={Classes["genre-classes"]}>
+            {genreData.map((data, key) => {
+              return <button key={key}>{data}</button>;
+            })}
+          </div>
+        </section>
+        <section className={Classes["toprated-section"]}></section>
+      </main>
+    </>
+  );
 };
 
 export default SideBody;
