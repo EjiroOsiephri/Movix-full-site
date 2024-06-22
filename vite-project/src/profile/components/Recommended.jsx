98413c8095e03e39d4dc515c0c29cf7ac3effa0f
ejiro osiephri <osiephriejiro765@gmail.com>
2024-06-22 09:27:56 +0100

    Fix: chenged the usage of the animation div and used it in another context

--- vite-project/src/profile/components/Recommended.jsx
+++ vite-project/src/profile/components/Recommended.jsx
@@ -3,6 +3,7 @@ import Classes from "./Recommended.module.scss";
 
 const Recommended = () => {
   const [recommended, setRecommended] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     const url = "http://localhost:8000/api/movies"; // Your backend URL
@@ -10,9 +11,11 @@ const Recommended = () => {
     try {
       const response = await fetch(url);
       const result = await response.json();
-      setRecommended(result.films); // Adjust based on the API response structure
+      setRecommended(result.films);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+      setIsLoading(true);
     }
   };
 
@@ -21,26 +24,44 @@ const Recommended = () => {
   }, []);
 
   return (
-    <main className={Classes["fetchData-main"]}>
-      <h1>Recommended</h1>
-      <section className={Classes["fetchData-section"]}>
-        {recommended?.map((data, id) => (
-          <section className={Classes["fetchData-div"]} key={id}>
-            <img
-              src={data.images?.poster[1]?.medium.film_image}
-              alt={data.film_name}
-            />
-            <div className={Classes["fetchData"]}>
-              <div>
-                <h1>{data.film_name}</h1>
-                <h4>{data.release_dates[0].release_date}</h4>
-              </div>
-              <h5>{data.age_rating[0].rating}</h5>
-            </div>
+    <>
+      {isLoading ? (
+        <div className={Classes["container"]}>
+          <div className={Classes["loading-container"]}>
+            <div
+              className={`${Classes["loading-bar"]} ${Classes["gradient-1"]} ${Classes["animate-loading"]}`}
+            ></div>
+            <div
+              className={`${Classes["loading-bar"]} ${Classes["gradient-2"]} ${Classes["animate-loading"]}`}
+            ></div>
+            <div
+              className={`${Classes["loading-bar"]} ${Classes["gradient-3"]} ${Classes["animate-loading"]}`}
+            ></div>
+          </div>
+        </div>
+      ) : (
+        <main className={Classes["fetchData-main"]}>
+          <h1>Recommended</h1>
+          <section className={Classes["fetchData-section"]}>
+            {recommended?.map((data, id) => (
+              <section className={Classes["fetchData-div"]} key={id}>
+                <img
+                  src={data.images?.poster[1]?.medium.film_image}
+                  alt={data.film_name}
+                />
+                <div className={Classes["fetchData"]}>
+                  <div>
+                    <h1>{data.film_name}</h1>
+                    <h4>{data.release_dates[0].release_date}</h4>
+                  </div>
+                  <h5>{data.age_rating[0].rating}</h5>
+                </div>
+              </section>
+            ))}
           </section>
-        ))}
-      </section>
-    </main>
+        </main>
+      )}
+    </>
   );
 };
 
--- vite-project/src/profile/components/Trending.jsx
+++ vite-project/src/profile/components/Trending.jsx
@@ -3,6 +3,7 @@ import Classes from "./Trending.module.scss";
 
 const Trending = () => {
   const [trendingData, setTrendingData] = useState(null);
+  const [loading, setIsLoading] = useState(true);
 
   const fetchData = async () => {
     try {
@@ -22,10 +23,10 @@ const Trending = () => {
 
       const data = await response.json();
       setTrendingData(data);
-
-      console.log(data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+      setIsLoading(true);
     }
   };
 
@@ -35,40 +36,39 @@ const Trending = () => {
 
   return (
     <>
-      <h1 className={Classes["trending-texts"]}>Trending</h1>
-      <main className={Classes["main-trending-container"]}>
-        {trendingData?.results?.map((data) => (
-          <div className={Classes["mappedDivTexts"]} key={data.id}>
-            <img
-              src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}
-              alt={data.name}
-            />
-            <section>
-              <h1>{data.original_title || data.name}</h1>
-              <h4>{data.vote_average}</h4>
-            </section>
+      {loading ? (
+        <div className={Classes["container"]}>
+          <div className={Classes["loading-container"]}>
+            <div
+              className={`${Classes["loading-bar"]} ${Classes["gradient-1"]} ${Classes["animate-loading"]}`}
+            ></div>
+            <div
+              className={`${Classes["loading-bar"]} ${Classes["gradient-2"]} ${Classes["animate-loading"]}`}
+            ></div>
+            <div
+              className={`${Classes["loading-bar"]} ${Classes["gradient-3"]} ${Classes["animate-loading"]}`}
+            ></div>
           </div>
-        ))}
-      </main>
-      <div className={Classes["container"]}>
-        <div className={Classes["loading-container"]}>
-          <div
-            className={`${Classes["loading-bar"]} ${Classes["gradient-1"]} ${Classes["animate-loading"]}`}
-          ></div>
-          <div
-            className={`${Classes["loading-bar"]} ${Classes["gradient-2"]} ${Classes["animate-loading"]}`}
-          ></div>
-          <div
-            className={`${Classes["loading-bar"]} ${Classes["gradient-3"]} ${Classes["animate-loading"]}`}
-          ></div>
-          <div
-            className={`${Classes["loading-bar"]} ${Classes["gradient-2"]} ${Classes["animate-loading"]}`}
-          ></div>
-          <div
-            className={`${Classes["loading-bar"]} ${Classes["gradient-1"]} ${Classes["animate-loading"]}`}
-          ></div>
         </div>
-      </div>
+      ) : (
+        <section>
+          <h1 className={Classes["trending-texts"]}>Trending</h1>
+          <main className={Classes["main-trending-container"]}>
+            {trendingData?.results?.map((data) => (
+              <div className={Classes["mappedDivTexts"]} key={data.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}
+                  alt={data.name}
+                />
+                <section>
+                  <h1>{data.original_title || data.name}</h1>
+                  <h4>{data.vote_average}</h4>
+                </section>
+              </div>
+            ))}
+          </main>
+        </section>
+      )}
     </>
   );
 };
